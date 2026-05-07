12cc3b4bab3295318bbf06bd910c2e31ed2a25e5
HeroHunter <abramscarter1999@gmail.com>
2026-05-07 17:24:03 -0400

    improve orb

--- scripts/game/attacks/orb.gd
+++ scripts/game/attacks/orb.gd
@@ -1,13 +1,15 @@
 extends "res://scripts/autoloads/attack_methods.gd"
 
-const DAMAGE = 55
-const KNOCKBACK = 1250
+const DAMAGE = 25
+const KNOCKBACK = 250
+const COOLDOWN = 0.45
+
 var checking = true
 
 func _process(_delta):
 	checking = true
 	
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().create_timer(COOLDOWN).timeout
 
 func _on_area_entered(area: Area2D) -> void:
 	deal_damage(area, DAMAGE, KNOCKBACK)
