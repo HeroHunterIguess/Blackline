36dc23ad789ba8e690823a223cae4e9686b804e1
HeroHunter <abramscarter1999@gmail.com>
2026-05-10 15:58:10 -0400

    initialize upgrade paths

--- scripts/ui/upgrade_menu.gd
+++ scripts/ui/upgrade_menu.gd
@@ -1,5 +1,13 @@
 extends CanvasLayer
 
+### upgrade paths ###
+const melee_upgrades = ["basic", "long_melee"] # long_melee not implimented
+const burst_upgrades = ["burst", "ground_slam", "super_slam"] # super_slam not implimented
+const ranged_upgrades = ["laser_gun", "grenade", "frag_grenade"] # none of these are implimented
+
+const movement_upgrades = ["dash", "double_jump", "air_freeze", "triple_jump"]
+const repeating_upgrades = ["health_increase", "melee_damage_increase", "ranged_damage_increase", "burst_damage_increase"] # none of these are implimented
+
 func close():
 	get_tree().paused = false
 	self.queue_free()
