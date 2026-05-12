a6cb94b38db3aa6c55b1185955b17e6d74b8d8df
HeroHunter <abramscarter1999@gmail.com>
2026-05-12 17:05:31 -0400

    initialize rng in upgrade menu

--- scripts/ui/upgrade_menu.gd
+++ scripts/ui/upgrade_menu.gd
@@ -8,6 +8,8 @@ const ranged_upgrades = ["laser_gun", "grenade", "frag_grenade"] # none of these
 const movement_upgrades = ["dash", "double_jump", "air_freeze", "triple_jump"]
 const repeating_upgrades = ["health_increase", "melee_damage_increase", "ranged_damage_increase", "burst_damage_increase"] # none of these are implimented
 
+var rng = RandomNumberGenerator.new()
+
 func close():
 	get_tree().paused = false
 	self.queue_free()
@@ -18,9 +20,13 @@ func _ready():
 
 ### get upgrades ###
 func _on_upgrade_1_button_down() -> void:
+	# chose random upgrade path
+	var random_choice = rng.randi_range(1, 3)
 	
 	close()
 
 func _on_upgrade_2_button_down() -> void:
+	# chose random buff upgrade path
+	var random_number = rng.randi_range(1, 2)
 	
 	close()
