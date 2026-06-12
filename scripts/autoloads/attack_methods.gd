de524fabe205868f6209510acc46be6753462a3b
TheMagmaKing2 <149970338+TheMagmaKing2@users.noreply.github.com>
2026-06-12 15:55:20 -0400

    Improved if syntax
    changed the if structure in knockback and equiped slam as well

--- scripts/autoloads/attack_methods.gd
+++ scripts/autoloads/attack_methods.gd
@@ -5,10 +5,9 @@ func deal_damage(area, damage, kb):
 	if !object.is_in_group("player") && !area.is_in_group("range_detection"):
 		if object.has_method("take_damage"):
 			object.health = object.take_damage(object.health, damage)
-			
-		if area.to_local(self.global_position).x >= 0:
-			if object.has_method("take_kb"):
-				object.take_kb(kb, false)
-		else: 
-			if object.has_method("take_kb"):
-				object.take_kb(kb, true)
+		
+		if object.has_method("take_kb"):	
+			if area.to_local(self.global_position).x >= 0:
+					object.take_kb(kb, false)
+			else:
+					object.take_kb(kb, true)
