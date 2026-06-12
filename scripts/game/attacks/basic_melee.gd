bb34cb32c671c9596b050827ce1273be72ce4dbc
TheMagmaKing2 <149970338+TheMagmaKing2@users.noreply.github.com>
2026-06-12 15:42:54 -0400

    Make Speed Constant
    made a constant speed variable

--- scripts/game/attacks/basic_melee.gd
+++ scripts/game/attacks/basic_melee.gd
@@ -2,6 +2,7 @@ extends "res://scripts/autoloads/attack_methods.gd"
 
 const DAMAGE = 50
 const KNOCKBACK = 270
+const SPEED = 0.8
 
 func _on_area_entered(area: Area2D) -> void:
 	deal_damage(area, DAMAGE, KNOCKBACK)
@@ -9,8 +10,8 @@ func _on_area_entered(area: Area2D) -> void:
 # move in direction of attack
 func _process(_delta):
 	if get_tree().root.find_child("player",true,false).to_local(self.global_position).x >= 0:
-		$Attack.global_position.x += 0.8
-		$CollisionShape2D.global_position.x += 0.8
+		$Attack.global_position.x += SPEED
+		$CollisionShape2D.global_position.x += SPEED
 	else:
-		$Attack.global_position.x -= 0.8
-		$CollisionShape2D.global_position.x -= 0.8
+		$Attack.global_position.x -= SPEED
+		$CollisionShape2D.global_position.x -= SPEED
