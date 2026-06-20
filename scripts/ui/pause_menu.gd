9db435a1615ffb77b15be202e908150709ca73a6
HeroHunter <abramscarter1999@gmail.com>
2026-06-20 16:38:07 -0400

    fix pause menu closing

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -1,10 +1,18 @@
 extends CanvasLayer
 
-func _process(_d):
+var close_cooldown = 0.5
+
+func _process(delta):
+	# lower cooldown
+	if get_tree().paused == true:
+		close_cooldown -= 1 * delta
+	
 	# close pause menu
-	if Input.is_action_just_pressed("pause"):
-		pass
+	if Input.is_action_just_pressed("pause") && close_cooldown <= 0:
+		get_tree().paused = false
+		close_cooldown = 0.5
 
 
 func _on_resume_button_pressed() -> void:
 	get_tree().paused = false
+	close_cooldown = 0.5
